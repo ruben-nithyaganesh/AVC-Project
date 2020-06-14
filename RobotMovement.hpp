b7a5ef16e483fcd557a4a95ff3fd9f233489ebbf
ruben-nithyaganesh <66393567+ruben-nithyaganesh@users.noreply.github.com>
2020-06-14 21:06:27 +1200

    Updated RobotMovement.hpp for challenge

--- RobotMovement.hpp
+++ RobotMovement.hpp
@@ -1,17 +1,14 @@
 class RobotMovement{
 	public:
-		void test();
-		void setTurn(double error);
+		void setTurn(errors error);
 		void robotStop();
-		void robotForward(double speed);
 	private:
-		wheelSpeed calcTurnSpeed(double error);
-		
+		wheelSpeed calcTurnSpeed(errors error);
 		double speed = 20; //max speed of each wheel
 };
 
 //Sets wheel speed
-void RobotMovement::setTurn(double error){
+void RobotMovement::setTurn(errors error){
 				
 	wheelSpeed wheelTurn = calcTurnSpeed(error);				//Get speeds for each wheel, pass values into wheelTurn struct
 	std::cout<<"vLeft = "<<wheelTurn.wheelLeft<<" vRight = "<<wheelTurn.wheelRight<<std::endl;
@@ -25,25 +22,39 @@ void RobotMovement::robotStop(){
 }
 
 //Calculates speed of each wheel depending on error value
-wheelSpeed RobotMovement::calcTurnSpeed(double error){ 
+wheelSpeed RobotMovement::calcTurnSpeed(errors error){ 
 				wheelSpeed wheelTurn;
-				if(error > 0){          // >0 means needs to turn right
+				errors error1 = error;
+				
+				if(error1.error > 0){          // >0 means needs to turn right
 						wheelTurn.wheelRight = speed  ;
-						wheelTurn.wheelLeft = speed - (error*(0.2*(0.1*speed)));
+						wheelTurn.wheelLeft = speed - (error1.error*(0.02*speed));
 				} 
-			   else if(error < 0){  // <0 means needs to turn left
-						wheelTurn.wheelRight = speed + (error*(0.2*(0.1*speed)));
+			   else if(error1.error < 0){  // <0 means needs to turn left
+						wheelTurn.wheelRight = speed + (error1.error*(0.02*speed));
 						wheelTurn.wheelLeft= speed;					
 				}
 				
-			  else if(error != 0){ //If there are no white pixels, turn on the spot
+			  else if(error1.error != 0){ //If there are no white pixels, try to guide with red pixels
+					if(error1.redError > 0){
+							wheelTurn.wheelRight = speed;
+							wheelTurn.wheelLeft = speed - (error1.redError*(0.02*speed));
+					}
+					else if(error1.redError < 0){
+						wheelTurn.wheelRight = speed + (error1.redError*(0.02*speed));
+						wheelTurn.wheelLeft= speed;							
+					}
+				    else if(error1.redError != 0){ //if no red or white, turn around
 						wheelTurn.wheelRight = speed;
-						wheelTurn.wheelLeft= 0;						  
+						wheelTurn.wheelLeft= 0;	
+						
+					}
+					  
 			  }
 				
 			  else{
 						wheelTurn.wheelRight = speed;
-						wheelTurn.wheelLeft= speed;						   
+						wheelTurn.wheelLeft = speed;						   
 			   }
 
 			return wheelTurn;
