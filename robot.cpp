7a152b98da4f60380f802c01dd1a1e8331eda09f
ruben-nithyaganesh <66393567+ruben-nithyaganesh@users.noreply.github.com>
2020-06-18 23:29:23 +1200

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -1,23 +1,33 @@
-
 #include "robot.hpp"
 #include "robotMovement.hpp"
 #include "imageProcessor.hpp"
 
 
-
 int main(){
 	if (initClientRobot() !=0){
-		std::cout<<" Error initializing robot"<<std::endl;			
+		std::cout<<" Error initializing robot"<<std::endl;
+				
+	}
+	
+	std::string str = "";
+	str = returnMazeType();
+	std::cout<<"File: "<<str<<std::endl;
+	int i;
+	if(returnMazeType() == "mazeFile,challenge.txt"){
+			i = 0;
+	}
+	else{
+			i = 1;
 	}
 	ImageProcessor image;
-	RobotMovement robot;
+	RobotMovement robot(i);
 	
     takePicture();
     SavePPMFile("i0.ppm",cameraView);
   
     while(1){
 	  takePicture();
-	  robot.setTurn(image.processor(cameraView));     
+	  robot.setTurn(image.processor(cameraView), image.getRedPaths(cameraView));     
       usleep(10000);
       
 	} //while
