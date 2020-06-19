60f2c6c7810088d77ca4ec1cbf517e60e9432392
ruben-nithyaganesh <66393567+ruben-nithyaganesh@users.noreply.github.com>
2020-06-19 17:17:36 +1200

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -1,23 +1,32 @@
-
 #include "robot.hpp"
 #include "robotMovement.hpp"
 #include "imageProcessor.hpp"
 
 
-
 int main(){
 	if (initClientRobot() !=0){
-		std::cout<<" Error initializing robot"<<std::endl;			
+		std::cout<<" Error initializing robot"<<std::endl;
 	}
+	
+	int i;
+	//get the first line from config.txt in AVC_server, and look to see if the maze is set to challenge
+	if(returnMazeType() == "mazeFile,challenge.txt"){
+			i = 0;
+	}
+	else{
+			i = 1;
+	}
+	//make instances of ImageProcessor and RobotMovement
 	ImageProcessor image;
-	RobotMovement robot;
+	RobotMovement robot(i); //i is passed in, representing whether or not the maze is set to challenge
 	
     takePicture();
     SavePPMFile("i0.ppm",cameraView);
   
     while(1){
+	//program loops forever, taking a picture, then calling setTurn in RobotMovement, passing in error and path values calculated with ImageProcessor
 	  takePicture();
-	  robot.setTurn(image.processor(cameraView));     
+	  robot.setTurn(image.processor(cameraView), image.getWhitePaths(cameraView), image.getRedPaths(cameraView));     
       usleep(10000);
       
 	} //while
